07baf765c5b72ad04d462a9376b26a0a96e95581
Or Aviram <or2.aviram@gmail.com>
2017-08-27 19:51:19 +0300

    Set up the window.

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Numerics;
+using SDL2;
+using SharpVulkan;
 
 namespace LearningCSharp
 {
@@ -6,7 +9,20 @@ namespace LearningCSharp
     {
         static void Main()
         {
-
+            IntPtr window = SDL.SDL_CreateWindow("Vulkan Sandbox", SDL.SDL_WINDOWPOS_UNDEFINED, SDL.SDL_WINDOWPOS_UNDEFINED, 1000, 500, SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+            bool running = true;
+            while (running)
+            {
+                while (SDL.SDL_PollEvent(out SDL.SDL_Event e) != 0)
+                {
+                    if (e.type == SDL.SDL_EventType.SDL_QUIT)
+                    {
+                        running = false;
+                        break;
+                    }
+                }
+            }
+            SDL.SDL_DestroyWindow(window);
         }
     }
 }
