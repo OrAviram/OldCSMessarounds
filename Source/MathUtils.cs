1ca69a5e792698a8c2d2576d8803a2435b998cfa
Or Aviram <or2.aviram@gmail.com>
2024-02-09 16:58:59 +0200

    Uhhhhh so many things, just want to back it up

--- Source/MathUtils.cs
+++ Source/MathUtils.cs
@@ -4,6 +4,21 @@ namespace LearningCSharp
 {
     public static class MathUtils
     {
+        public static double IntPow(double b, int exponent)
+        {
+            double result = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                result *= b;
+            }
+            return result;
+        }
+
+        public static int GetDigit(int number, int digit, int b)
+        {
+            return (int)(Math.Floor(number / IntPow(b, digit)) - b * Math.Floor(number / IntPow(b, digit + 1)));
+        }
+
         public static uint Clamp(this uint value, uint min, uint max)
         {
             return Math.Max(min, Math.Min(max, value));
--- Source/Program.cs
+++ Source/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Numerics;
+using System.Reflection;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using NativeInteropEx;
@@ -70,15 +71,86 @@ namespace LearningCSharp
                 return string.Format("({0}, {1}, {2}, {3})", this[0], this[1], this[2], this[3]);
             }
         }
+        
+        /// <summary>
+        /// Returns a zero of f on [a, b] if it exists, and null otherwise.
+        /// </summary>
+        static float? FindZeroOn(float a, float b, Func<float, float> f)
+        {
+            if (Math.Sign(f(a)) == Math.Sign(f(b)))
+                return null;
+
+            float l = a;
+            float r = b;
+            float avg = (a + b) / 2;
+            while (Math.Abs(f(avg)) > 0.001f)
+            {
+                avg = (l + r) / 2;
+                if (f(r) * f(avg) > 0)
+                    r = avg;
+                else
+                    l = avg;
+            }
+            return (l + r) / 2;
+        }
+
+        static float Sqrt(float x) => (float)Math.Sqrt(x);
+        static float Sin(float x) => (float)Math.Sin(x);
 
         public static void Main()
         {
+            //Console.ReadKey();
+            //return;
+
+            /*
+            const int n = 1000000;
+            float s = 1;
+            int c = 1;
+            for (int i = 1; i < n; i++)
+            {
+                if (c == 1)
+                {
+                    s = 1 / (1 / s + 1);
+                    c = (int)(1 / s);
+                    continue;
+                }
+
+                if (c % 2 == 0)
+                    c /= 2;
+                else
+                    c = 3 * c + 1;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+            return;
+            
+            // NOTE: Does not work well with large intervals when there are many zeroes between.
+            //       I need to find a different way to check if there is a solution.
+
+            const float a = -100;
+            const float b = 100;
+
+            Func<float, float> f = x => x * x * x * x * Sin(x) * Sin(1/x) + 3 * x - 5 + 7 * Sin(x) + 1/(x * x);
+            float? x0 = FindZeroOn(a, b, x => f(x));
+
+            if (x0.HasValue)
+            {
+                Logger.Log("Solution = " + x0 + ".");
+                Logger.Log("Verification: f(solution) = " + f(x0.Value) + ".");
+            }
+            else
+                Logger.Log("No solution on [" + a + ", " + b + "].");
+
+            Console.ReadKey();
+            return;
+            */
+
             Logger.Log("Information about the SIMD stuff:", ConsoleColor.Yellow);
             Console.WriteLine(System.Numerics.Vector.IsHardwareAccelerated);
             Console.WriteLine(Vector<float>.Count);
             Console.WriteLine(Marshal.SizeOf<Vector>());
 
-            Logger.Log("Actual Tests (first Microsoft's, then mine):", ConsoleColor.Yellow);
+            Logger.Log("Actual Tests (first Microsoft's, then mine, then mine with no SIMD):", ConsoleColor.Yellow);
 
             Stopwatch watch = new Stopwatch();
             const long iterationsCount = 99999999;
@@ -109,8 +181,23 @@ namespace LearningCSharp
             watch.Stop();
             Logger.Log(watch.ElapsedTicks, ConsoleColor.Red);
 
+            watch.Reset();
+
+            Vector result3 = new Vector();
+            watch.Start();
+            for (long i = 0; i < iterationsCount; i++)
+            {
+                Vector v1 = new Vector(i, 2 * i, 3 * i, 4 * i);
+                Vector v2 = new Vector(5 * i, 6 * i, -1 * i, 0.534f * i);
+
+                result3 = Vector.Add(v1, v2);
+            }
+            watch.Stop();
+            Logger.Log(watch.ElapsedTicks, ConsoleColor.Red);
+
             Console.WriteLine(result1);
             Console.WriteLine(result2);
+            Console.WriteLine(result3);
             
             Console.ReadKey(true);
             return;
